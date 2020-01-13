ee1853231366fd81087fcf231f08e97d8b1f41a2
Edvanio Filho <53785275+EdFilho@users.noreply.github.com>
2020-01-13 14:04:33 -0300

    Add files via upload

---
+++ db_connection.py
@@ -0,0 +1,46 @@
+encoding = 'UTF-8'
+import cx_Oracle
+
+def db_connection():
+
+        #Oracle database connection
+        #Replace with your user, password, ip andress, port and database used
+        ora_conn = cx_Oracle.connect('user/password@ip:port/database')
+        ora_cur = ora_conn.cursor()
+
+        #version connection
+        print(f'version connection: {ora_conn.version}\n')
+
+        #open a file
+        archive = open('autonomos1.txt', 'w')
+
+        #SQL command
+        sql_command = """INSERT YOUR SQL COMMAND"""
+
+        #execute your command in database
+        ora_cur.execute(sql_command)
+
+        #search for result of command
+        result = ora_cur.fetchone()
+
+        #if don't have result
+        if result == None:
+                print ("Don't have data.")
+        else:
+                #if return result
+                while result:
+                        #write in file txt
+                        for x in result:
+                                archive.write(f'{str(x)}|')
+                        archive.write('\n')
+                        #print result on screen
+                        print (result)
+                        result = ora_cur.fetchone()
+
+        #close connection, cursor and file
+        ora_cur.close()
+        ora_conn.close()
+        archive.close()
+
+if(__name__ == '__main__'):
+        db_connection()
